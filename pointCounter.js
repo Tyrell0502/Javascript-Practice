f662402e22b487d67a864a187f2cd5346af46750
Tyrell Brantley <51008312+Tyrell0502@users.noreply.github.com>
2021-06-19 11:35:07 -0400

    Add files via upload

---
+++ pointCounter.js
@@ -0,0 +1,7 @@
+function pointCounter() {
+    var twoPointers = prompt('How many two pointers were scored?: ');
+    var threePointers = prompt('How many three pointers were scored?: ');
+
+    var totalPoints = twoPointers + threePointers;
+    return totalPoints;
+}
